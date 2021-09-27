3405d8fb48152e71be73fdc538be4177420c15a3
hamongkhang <79957186+hamongkhang@users.noreply.github.com>
2021-09-27 13:29:56 +0700

    Revert "Be#34555"

--- back-end/app/Models/User.php
+++ back-end/app/Models/User.php
@@ -7,10 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -20,21 +18,14 @@ class User extends Authenticatable implements JWTSubject
      * @var string[]
      */
     protected $fillable = [
-        'fullName',
+        'name',
         'email',
         'password',
         'avatar',
         'updated_at',
-        'created_at',
-        'sex',
-        'address',
-        'birthday',
-        'phone',
-        'linkFB',
-        'nameAccount',
-        'status',
+        'created_at'
     ];
-   
+
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -53,21 +44,4 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-      /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier() {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims() {
-        return [];
-    }    
 }
--- back-end/config/app.php
+++ back-end/config/app.php
@@ -174,8 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-
 
     ],
 
@@ -230,8 +228,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
