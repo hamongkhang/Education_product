919af4f4f11ffd25dba533ffd6719cdcf6da75dc
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2021-09-27 13:41:04 +0700

    fix:api

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -34,7 +34,7 @@ Route::post('/users/userProfile', [App\Http\Controllers\UsersController::class, 
 Route::post('/users/changePassword', [App\Http\Controllers\UsersController::class, 'changePassword'])->name('user.changePassword');
 Route::post('/users/getCodeForgotPassword', [App\Http\Controllers\UsersController::class, 'getCodeForgotPassword'])->name('user.getCodeForgotPassword');
 Route::post('/users/changePasswordForgot', [App\Http\Controllers\UsersController::class, 'changePasswordForgot'])->name('user.changePasswordForgot');
-
+/* Api Payment */
 Route::post('/payment/momoPayment', [App\Http\Controllers\PaymentController::class, 'momoPayment'])->name('payment.momoPayment');
 Route::post('/payment/checkResult', [App\Http\Controllers\PaymentController::class, 'checkResult'])->name('payment.checkResult');
 Route::post('/payment/atmPayment', [App\Http\Controllers\PaymentController::class, 'atmPayment'])->name('payment.atmPayment');
