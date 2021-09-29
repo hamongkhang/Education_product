1ed7359043f09aa61d30b68aff5779f9e3d17356
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2021-09-29 07:33:19 +0700

    feat: api getITinTeach

---
+++ back-end/app/Http/Controllers/ITinTeachController.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UsersResource;
+use App\Jobs\SendEmail;
+use App\Models\User;
+use App\Models\Cart;
+use App\Models\ITinTeach;
+use App\Models\UserCode;
+use App\Models\UserCourse;
+use App\Models\ForgotCode;
+use App\Models\momoOrderDetail;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+class ITinTeachController extends Controller
+{
+     /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['onLogin','getITinTeach', 'onRegister','getCode','getCodeForgotPassword','changePasswordForgot']]);
+    }
+    
+      /**
+     * @SWG\GET(
+     *     path="api/ITinTeach/getITinTeach",
+     *     description="get file IT i Teach",
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Successfully",
+     *         @SWG\Schema(
+     *             @SWG\Property(property="id", type="integer"),
+     *             @SWG\Property(property="name", type="string"),
+     *             @SWG\Property(property="description", type="string"),
+     *             @SWG\Property(property="image", type="string"),
+     *             @SWG\Property(property="author", type="string"),
+     *             @SWG\Property(property="file", type="string"),
+     *             @SWG\Property(property="path", type="string"),
+     *             @SWG\Property(property="status", type="string"),
+     *             @SWG\Property(property="created_at", type="datetime"),
+     *             @SWG\Property(property="updated_at", type="datetime"),
+     *            )
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Missing Data"
+     *     )
+     * )
+     */
+    public function getITinTeach(Request $request)
+    {
+            $data = DB::table('ITinTeach')->get();
+            return response()->json(['data' => $data]);
+    }
+}
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -46,3 +46,5 @@ Route::post('/payment/momoPayment', [App\Http\Controllers\PaymentController::cla
 Route::post('/payment/checkResult', [App\Http\Controllers\PaymentController::class, 'checkResult'])->name('payment.checkResult');
 Route::post('/payment/atmPayment', [App\Http\Controllers\PaymentController::class, 'atmPayment'])->name('payment.atmPayment');
 //Route::post('/payment/checkResultATM', [App\Http\Controllers\PaymentController::class, 'checkResultATM'])->name('payment.checkResultATM');
+/* Api ITinTeach */
+Route::get('/ITinTeach/getITinTeach', [App\Http\Controllers\ITinTeachController::class, 'getITinTeach'])->name('ITinTeach.getITinTeach');
