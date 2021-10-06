8071e35342b1a6687bfe8bfd98d1ac5e5905c59d
NguyenQuan <hamongkhang@gmail.com>
2021-10-06 14:23:15 +0700

    fix: user front-end+back-end

--- front-end/src/components/profile/avt.jsx
+++ front-end/src/components/profile/avt.jsx
@@ -3,56 +3,56 @@ import {useHistory} from 'react-router-dom'
 import moment from 'moment';
 
 const Avt = (props) => {
-    const $link="http://localhost:8000/upload/images/avatar/";
-    const [profile, setProfile] = useState([]);
-    const [option,setOption] =useState([]);
-    const history = useHistory();
-    const $token=localStorage.getItem('access_token');
-    const requestOptions = {
-        method: 'POST',
-        headers: {"Authorization": `Bearer `+$token}
-    };
-    useEffect(() => {
-      if($token){
-        fetch("http://localhost:8000/api/users/userProfile",requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            setProfile(data);
-            if(data.sex==="female"){
-                    setOption({
-                    option1:<option value="">Khác</option>,
-                    option2:<option value="">Nam</option>,
-                    option3:<option selected value="">Nữ</option>
-                    });
-            }
-            else if(data.sex==="male"){
-                setOption({
-                option1:<option value="">Khác</option>,
-                option2:<option selected value="">Nam</option>,
-                option3:<option value="">Nữ</option>
-                });
-        }
-        else{
-            setOption({
-                option1:<option selected value="">Khác</option>,
-                option2:<option value="">Nam</option>,
-                option3:<option value="">Nữ</option>
-                });
-        }
-        });
-        return () => {
-        }
-    }else{
-        alert("Bạn chưa đăng nhập")
-       history.push("dang-nhap")
-    }
-    }
+       const $link="http://localhost:8000/upload/images/avatar/";
+    // const [profile, setProfile] = useState([]);
+    // const [option,setOption] =useState([]);
+    // const history = useHistory();
+    // const $token=localStorage.getItem('access_token');
+    // const requestOptions = {
+    //     method: 'POST',
+    //     headers: {"Authorization": `Bearer `+$token}
+    // };
+    // useEffect(() => {
+    //   if($token){
+    //     fetch("http://localhost:8000/api/users/userProfile",requestOptions)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setProfile(data);
+    //         if(data.sex==="female"){
+    //                 setOption({
+    //                 option1:<option value="">Khác</option>,
+    //                 option2:<option value="">Nam</option>,
+    //                 option3:<option selected value="">Nữ</option>
+    //                 });
+    //         }
+    //         else if(data.sex==="male"){
+    //             setOption({
+    //             option1:<option value="">Khác</option>,
+    //             option2:<option selected value="">Nam</option>,
+    //             option3:<option value="">Nữ</option>
+    //             });
+    //     }
+    //     else{
+    //         setOption({
+    //             option1:<option selected value="">Khác</option>,
+    //             option2:<option value="">Nam</option>,
+    //             option3:<option value="">Nữ</option>
+    //             });
+    //     }
+    //     });
+    //     return () => {
+    //     }
+    // }else{
+    //     alert("Bạn chưa đăng nhập")
+    //    history.push("dang-nhap")
+    // }
+    // }
     
-    , []);
+    // , []);
     return (
         <div className="group">
             <div className="mx-auto relative w-64 h-64 md:w-80 md:h-80 border-red-600 border rounded-lg mb-10 md:mb-0" >
-                <img src={$link+profile.avatar}  className="w-full h-full mb-10 md:mb-0 object-cover rounded-lg" alt=""/>
+                <img src={$link+props.profile.avatar}  className="w-full h-full mb-10 md:mb-0 object-cover rounded-lg" alt=""/>
                 <label htmlFor="avt" className="w-4/5 text-center opacity-0 group-hover:opacity-100 block py-2 rounded-md bg-yellow-400 hover:bg-yellow-500 cursor-pointer text-15 font-semibold absolute bottom-5 transform left-1/2 -translate-x-1/2 duration-300 text-white"> <i className="fad fa-camera mr-2"></i> <span> Đổi ảnh</span></label>
                 <input type="file" id="avt" hidden/>
             </div>
--- front-end/src/components/profile/changeInfo.jsx
+++ front-end/src/components/profile/changeInfo.jsx
@@ -4,51 +4,51 @@ import moment from 'moment';
 
 
 const ChangeInfo = (props) => {
-    const [profile, setProfile] = useState([]);
-    const [option,setOption] =useState([]);
-    const history = useHistory();
-    const $token=localStorage.getItem('access_token');
-    const requestOptions = {
-        method: 'POST',
-        headers: {"Authorization": `Bearer `+$token}
-    };
-    useEffect(() => {
-      if($token){
-        fetch("http://localhost:8000/api/users/userProfile",requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            setProfile(data);
-            if(data.sex==="female"){
-                    setOption({
-                    option1:<option value="">Khác</option>,
-                    option2:<option value="">Nam</option>,
-                    option3:<option selected value="">Nữ</option>
-                    });
-            }
-            else if(data.sex==="male"){
-                setOption({
-                option1:<option value="">Khác</option>,
-                option2:<option selected value="">Nam</option>,
-                option3:<option value="">Nữ</option>
-                });
-        }
-        else{
-            setOption({
-                option1:<option selected value="">Khác</option>,
-                option2:<option value="">Nam</option>,
-                option3:<option value="">Nữ</option>
-                });
-        }
-        });
-        return () => {
-        }
-    }else{
-        alert("Bạn chưa đăng nhập")
-       history.push("dang-nhap")
-    }
-    }
+    // const [profile, setProfile] = useState([]);
+    // const [option,setOption] =useState([]);
+    // const history = useHistory();
+    // const $token=localStorage.getItem('access_token');
+    // const requestOptions = {
+    //     method: 'POST',
+    //     headers: {"Authorization": `Bearer `+$token}
+    // };
+    // useEffect(() => {
+    //   if($token){
+    //     fetch("http://localhost:8000/api/users/userProfile",requestOptions)
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setProfile(data);
+    //         if(data.sex==="female"){
+    //                 setOption({
+    //                 option1:<option value="">Khác</option>,
+    //                 option2:<option value="">Nam</option>,
+    //                 option3:<option selected value="">Nữ</option>
+    //                 });
+    //         }
+    //         else if(data.sex==="male"){
+    //             setOption({
+    //             option1:<option value="">Khác</option>,
+    //             option2:<option selected value="">Nam</option>,
+    //             option3:<option value="">Nữ</option>
+    //             });
+    //     }
+    //     else{
+    //         setOption({
+    //             option1:<option selected value="">Khác</option>,
+    //             option2:<option value="">Nam</option>,
+    //             option3:<option value="">Nữ</option>
+    //             });
+    //     }
+    //     });
+    //     return () => {
+    //     }
+    // }else{
+    //     alert("Bạn chưa đăng nhập")
+    //    history.push("dang-nhap")
+    // }
+    // }
     
-    , []);
+    // , []);
 
     return(
     <div className="bg-indigo-100 p-5 shadow-lg">
@@ -59,40 +59,40 @@ const ChangeInfo = (props) => {
             <div className="md1:flex md1:space-x-4">
                 <div className="w-full mb-4">
                     <label htmlFor="name" className="block w-full mb-0.5">Họ và tên</label>
-                    <input id="name" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={profile.fullName} required/>
+                    <input id="name" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={props.profile.fullName} required/>
                 </div>
                 <div className="w-full mb-4">
                     <label htmlFor="facebookUrl" className="block w-full mb-0.5">Link Facebook</label>
-                    <input id="facebookUrl" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={profile.linkFB} required/>
+                    <input id="facebookUrl" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={props.profile.linkFB} required/>
                 </div>
             </div>
             <div className="md1:flex md1:space-x-4">
                 <div className="w-full mb-4">
                     <label htmlFor="phone" className="block w-full mb-0.5">Số điện thoại</label>
-                    <input id="phone" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={profile.phone} required/>
+                    <input id="phone" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={props.profile.phone} required/>
                 </div>
                 <div className="w-full mb-4">
                     <label htmlFor="gender" className="block w-full mb-0.5">Giới tính</label>
                     <select name="gender"  id="s" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2">
-                        {option.option1}
-                        {option.option2}
-                        {option.option3}
+                        {props.option.option1}
+                        {props.option.option2}
+                        {props.option.option3}
                        
                     </select>
                 </div>
                 <div className="w-full mb-4">
                     <label htmlFor="birthday" className="block w-full mb-0.5">Sinh nhật</label>
-                    <input id="birthday" type="date"  value={moment(profile.birthday).format("YYYY-MM-DD")} className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2"  required/>
+                    <input id="birthday" type="date"  value={moment(props.profile.birthday).format("YYYY-MM-DD")} className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2"  required/>
                 </div>
             </div>
             <div className="md1:flex md1:space-x-4">
                 <div className="w-full mb-4">
                     <label htmlFor="address" className="block w-full mb-0.5">Địa chỉ</label>
-                    <input id="address" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={profile.address} required/>
+                    <input id="address" type="text" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={props.profile.address} required/>
                 </div>
                 <div className="w-full mb-4">
                     <label htmlFor="email" className="block w-full mb-0.5 ">Email</label>
-                    <input id="email" type="email" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={profile.email} required/>
+                    <input id="email" type="email" className="px-4 py-2 w-full focus:border-indigo-500 border-gray-300 hover:border-gray-400 rounded outline-none border-2" value={props.profile.email} required/>
                 </div>
             </div>
             <div className="text-right">
