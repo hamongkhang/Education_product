1c54e41cde83e286f90d5e7fd5e7a1a60be3b8d9
Phat0191 <tranphat0191@gmail.com>
2021-10-14 17:55:36 +0700

    fix: see more and see less document

--- front-end/src/components/List_document.js
+++ front-end/src/components/List_document.js
@@ -1,19 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Other_document.css";
 
 export const ListDocument = (props) => {
   const { list } = props;
+  const [show, setShow] = useState(5);
+  const [arr, setArr] = useState(list);
+  const [arr1, setArr1] = useState([]);
+  const [txt, setTxt] = useState("Xem thêm");
+  
+  useEffect(() => {
+    let arrTemp = arr;
+    let arr1Temp = arrTemp.content;
+    arrTemp = arr1Temp.slice(0, show - 1);
+    setArr1(arrTemp);
+    setTxt("Xem thêm");
+  }, []);
+
+
+  const handleShow = () => {
+    if (txt === "Xem thêm") {
+      setArr1(list.content);
+      setTxt("Ẩn bớt");
+      console.log(list)
+    }
+    else {
+      let arrTemp = arr;
+      let arr1Temp = arrTemp.content;
+      arrTemp = arr1Temp.slice(0, show - 1);
+      setArr1(arrTemp);
+      setTxt("Xem thêm");
+    }
+  };
 
   return (
-    <>
+        <>
       <div className="other-doc-right__block">
         <div className="other-doc-right__header">
           <div className="other-doc-right__title">
             <h4>{list.name}</h4>
+            
           </div>
         </div>
-        {list.content && list.content.length
-          ? list.content.map((items) => {
+        {arr1 && arr1.length
+          ? arr1.map((items) => {
               return (
                 <>
                   <div className="other-doc-right-content">
@@ -31,8 +60,8 @@ export const ListDocument = (props) => {
               );
             })
           : ""}
-        <div className="other-doc-right__seemore">
-          <p>Xem thêm</p>
+        <div className="other-doc-right__seemore" onClick={() => handleShow()}>
+          <p>{txt}</p>
         </div>
       </div>
     </>
