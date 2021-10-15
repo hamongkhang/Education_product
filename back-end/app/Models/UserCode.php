7bdc196016f8016cce46669aa9cda659a93d64fc
NguyenQuan <hamongkhang@gmail.com>
2021-10-15 14:05:38 +0700

    revert

---
+++ back-end/app/Models/UserCode.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+
+class UserCode extends Authenticatable implements JWTSubject
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $table="user_code";
+    protected $fillable = [
+        'fullName',
+        'email',
+        'password',
+        'updated_at',
+        'created_at',
+        'sex',
+        'address',
+        'birthday',
+        'phone',
+        'linkFB',
+        'nameAccount',
+        'code',
+    ];
+   
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+      /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    }    
+}
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-<<<<<<< HEAD
 import HomePages from './views/home'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
@@ -29,33 +28,6 @@ function App() {
           <Footer/>
         </Router>
       </div>
-=======
-import Home from './components/Home'
-import Login from './components/Login'
-import Register from './components/Register'
-import { Other_document } from './components/Other_document';
-function App() {
-  return (
-    <div className="container-fluid">
-      <Router>
-        <div className="menu-top">
-            <div className="btnHome">
-                <Link to="/" style={{backgroundImage:`url(${process.env.PUBLIC_URL + '/bg-menu-top.png'})`}}></Link>
-            </div>
-            <div className="menu">
-              <Link to="/login">Login</Link>
-              <Link to="/register">Register</Link>
-              <Link to='/other-document'>Other Document</Link>
-            </div>
-        </div>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path='/other-document' component={Other_document} />
-        </Switch>
-      </Router>
->>>>>>> Other_document
     </div>
   );
 }
