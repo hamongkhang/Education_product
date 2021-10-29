8e76083f8e78dacc3720d5ab836a205ded45f068
NguyenQuan <hamongkhang@gmail.com>
2021-10-29 16:58:37 +0700

    feat : final exam with client

---
+++ front-end/src/views/home/exam/Countdown.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const formatTime = (timer) => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)  
+    return (`${getHours} : ${getMinutes} : ${getSeconds}`)
+  }
+
+const Countdown = ({ seconds,timer }) => {
+        return (
+          <div className="app">
+            <h3>Thời gian làm bài</h3>
+            <div className='stopwatch-card'>
+              <p>{formatTime(timer)}</p>
+            </div>
+          </div>
+        );
+}
+
+export default Countdown;
+  
